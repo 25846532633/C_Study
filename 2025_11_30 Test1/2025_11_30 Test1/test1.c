90a5d1e934247d42ec0115485740ad8179d4a581
cao-gezhang <2076387719@qq.com>
2025-12-02 16:44:00 +0800

    ＂C的基础＂

---
+++ 2025_11_30 Test1/2025_11_30 Test1/test1.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//#include "add.c"
+//int main()
+//{
+//	printf("hello world");
+//	return 0;
+//}
+//
+//void test()
+//{
+//	static int i = 0;
+//	i++;
+//	printf("%d ", i);
+//}
+
+//extern int g_val;
+//int main()
+//{
+//	printf("%d", g_val);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 32; i <= 127; i++)
+//	{
+//		printf("%c ", i);
+//		if (i % 16 == 15)
+//			printf("\n");
+//	}
+//	return 0;
+//}
+
+//int n = 1000;
+//int main()
+//{
+//	int n = 10;
+//	{
+//		int n = 5;
+//	}
+//	printf("%d", n);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%s\n", "hello C");
+//	printf("hello c");
+//	return 0;
+//}
+
+int main()
+{
+	/*short s = 2;
+	int b = 10;
+	printf("%d\n", sizeof(s = b + 1));
+	printf("s = %d\n", s);*/
+	int x; float y;
+	//    -13.45e12# 0"
+	scanf("%d", &x);
+	printf("%d\n", x);
+	scanf("%f", &y);
+	printf("%f\n", y);
+	return 0;
+}

428489893a5a762960baa53945b58ecb63c4cb48
cao-gezhang <2076387719@qq.com>
2026-01-15 21:30:15 +0800

    “作业”

--- 2025_1_13 字符串和字符串函数/2025_1_13 字符串和字符串函数/my_strlen.c
+++ 2025_1_13 字符串和字符串函数/2025_1_13 字符串和字符串函数/my_strlen.c
@@ -1,7 +1,7 @@
-//#define _CRT_SECURE_NO_WARNINGS	
-//#include <string.h>
-//#include <stdio.h>
-//#include <assert.h>
+#define _CRT_SECURE_NO_WARNINGS	
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
 //
 ////int my_strlen(const char* str)
 ////{
@@ -92,10 +92,74 @@
 //}
 //
 //
-//int main()
+
+
+//int my_strcmp(const char* str1, const char* str2)
 //{
-//	char arr1[] = "abced";
-//	char arr2[] = "abcedl";
-//	printf("%d",my_strcmp(arr1, arr2));//-1
-//	return 0;
+//	assert(str1 && str2);
+//	while (*str1 == *str2)
+//	{
+//		if (*str1 == '\0')//str1结束 - 说明二者相同
+//			return 0;
+//		str1++; str2++;
+//	}
+//	return *str1 - *str2;//二者的ASCII码差值
 //}
+
+int my_strcmp(const char* src, const char* dst)
+{
+	int ret = 0;
+	assert(src != NULL);
+	assert(dst != NULL);
+	while (!(ret = *(unsigned char*)src - *(unsigned char*)dst) & *dst)
+	{
+		dst++; src++;
+	}
+	if (ret < 0)
+		return -1;
+	else if (ret > 0)
+		return 1;
+	else
+		return 0;
+}
+
+
+char* my_strstr(char* dest, const char* src)
+{
+	char* cp = dest;
+	char* str1, *str2;
+
+	while (*cp)
+	{
+		str1 = cp; str2 = src;
+		while (*str1 && *str2 && *str1 == *str2)
+		{
+			*str1++; *str2++;
+		}
+		if (!*str2)//找到对应子串
+			return cp;
+		cp++;
+	}
+	return NULL;
+}
+
+char* my_strcat(char* dest, const char* src)
+{
+	assert(dest && src);
+	char* ret = dest;
+	while (*++dest);
+	while (*dest++ = *src++);
+	return ret;
+}
+
+int main()
+{
+	char arr1[20] = "abced";
+	char arr2[] = "abcedl";
+	char* test = my_strcat(arr1, arr2);
+	printf("%d\n",my_strcmp(arr1, arr2));//-1
+	printf("%s\n", my_strstr(arr1, "bce"));
+	printf("%s\n", test);
+	return 0;
+}
+
